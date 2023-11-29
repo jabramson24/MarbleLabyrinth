b7850ddf120cc72a8fda82767a12b337a81dbe53
Arman Mohammadi <amoham42@armans-mbp-3.devices.brown.edu>
2023-11-29 02:36:49 -0500

    Threejs setup

---
+++ game/main.js
@@ -0,0 +1,20 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.6, 1200);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#233143");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Make Canvas Responsive
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
